7fa7f68ed02e03483975d77d195737db6dd5bc63
Kjiw0n <128016888+Kjiw0n@users.noreply.github.com>
2024-06-12 00:12:25 +0900

    [Style] 폴더구조 및 스타일 세팅 (#2)
    * style: 폴더 구조 세팅
    
    * style: theme, globalstyle 설정

---
+++ src/style/theme.ts
@@ -0,0 +1,11 @@
+const colors = {
+  transparentBackground: "rgba(255, 255, 255, 0.7)",
+  white: "#FFFFFF",
+  black: "#000000",
+  red: "#FF0000",
+  gray: "#808080",
+};
+
+const theme = { colors };
+
+export default theme;
