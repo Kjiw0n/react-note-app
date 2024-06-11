554937264c228debd362e6a95144ea2590b5a132
Kjiw0n <rwd0904@naver.com>
2024-06-12 03:44:49 +0900

    style: light/dark theme 색상 추가

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -14,16 +14,28 @@ export default theme;
 
 export const lightTheme = {
   colors: {
-    background: "#f0f0f0",
+    background: "rgba(255, 255, 255, 0.7)",
     text: "#000000",
     primary: "#6200ee",
+    white: "#FFFFFF",
+    black: "#000000",
+    red: "#FF0000",
+    gray: "#808080",
+    blue: "#3587F2",
+    lightgray: "#ddd",
   },
 };
 
 export const darkTheme = {
   colors: {
-    background: "#121212",
+    background: "rgba(18, 18, 18, 0.8)",
     text: "#ffffff",
     primary: "#bb86fc",
+    white: "#FFFFFF",
+    black: "#000000",
+    red: "#FF0000",
+    gray: "#808080",
+    blue: "#3587F2",
+    lightgray: "#ddd",
   },
 };
